d3d5ea08f631224b7c14d280d053ad81d43a1251
Abdifatah <abdifatah.101@outlook.com>
2021-07-10 11:19:36 +0100

    Feat: Added some API commands

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+$("#currentDay").text(moment().format("DD/M/YYYY"));
+
 var API_KEY = "e72b1512a96a15828d146af29009e625"
 
-function generateURL(city) {
+function generateEndpoint(city) {
     var NUMBER_OF_DAYS_TO_REQUEST = 5;
     return `api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=${NUMBER_OF_DAYS_TO_REQUEST}&appid=${API_key}`;
 
 }
+
+function makeRequest(endpoint) {
+    return fetch (endpoint).then(function(response) {
+        return response.json();
+    });
+
+}
+
+var city = "Birmingham";
+
+var endpoint = generateEndpoint(city)
+
+makeRequest(endpoint).then(function(weatherInfo){
+    
+})
